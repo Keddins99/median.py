0fe75230181fc4e2f123119725cc122da2b2747f
Keddins <eddinskeathan@gmail.com>
2023-11-08 20:46:34 -0800

    median.py

--- find_median.py
+++ find_median.py
@@ -1,20 +1,23 @@
-def find_median(numbers):
-    # sort the list of numbers in ascending order
-    numbers.sort()
-    # find the length of the list
-    n = len(numbers)
+def find_median(numbers: list) -> float:
+    if not numbers:
+        return None
 
-    # check if the list has an odd or even number of elements
-    if (n % 2 == 1):  # odd number of elements
-        # return the middle element as the median
-        return numbers[n // 2]
-    else:  # even number of elements
-        # calculate the average of the two middle elements as the median
-        middle1 = numbers[(n // 2) - 1]
-        middle2 = numbers[n // 2]
-        return (middle1 + middle2) / 2
+    sorted_numbers = sorted(numbers)
+    n = len(sorted_numbers)
 
+    if n % 2 == 0:
+        # If the number of elements is even, calculate the average of the middle two numbers
+        mid_left = sorted_numbers[n // 2 - 1]
+        mid_right = sorted_numbers[n // 2]
+        median = (mid_left + mid_right) / 2
+    else:
+        # If the number of elements is odd, the median is the middle number
+        median = sorted_numbers[n // 2]
 
-some_nums = [13, 7, -3, 82, 4]
-result = find_median(some_nums)
-print("Median:", result)
+    return median
+
+# Example usage
+if __name__ == "__main__":
+    values = [13, 7, -3, 82, 4]
+    result = find_median(values)
+    print(result)
